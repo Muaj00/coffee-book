74db15c4b26b2c56c86158493577d72f8f13b383
Muaj00 <muaj15-14389@diu.edu.bd>
2025-03-03 13:08:23 +0600

    footer added

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 
 const MainLayouts = () => {
@@ -8,8 +9,11 @@ const MainLayouts = () => {
               {/*Navbarvomponent  */}
               <NavBar></NavBar>
               {/*Dynamic Section  */}
+              <div className="min-h-[calc(100vh-232px)]">
               <Outlet></Outlet>
+              </div>
               {/*Footer Component  */}
+              <Footer></Footer>
         </div>
     );
 };
